d743610c50e254af004b9ac2328ff4303364c079
Varinder Singh <60591988+varinder139@users.noreply.github.com>
2020-03-27 19:04:09 +0530

    ConstantsShortCut.java file is part of the Shortcut Function
    we have complete all coding expect manifest file....

---
+++ ConstantsShortCut.java
@@ -0,0 +1,12 @@
+package varinder.study.learnexcelwithexamples;
+
+public class ConstantsShortCut {
+    public static final String count_of_cells_with_text = "Ctrl+A_Sellect_All";
+    public static final String vlookup = "Ctrl+D Fill the Content of the selected cell on top to bottom. CTRL + D (i.e. Ctrl+ Down)";
+    public static final String index_match = "Index Match";
+    public static final String offset = "Offset";
+    public static final String remove_spaces = "Remove Spaces";
+    public static final String two_way_lookup = "Two Way Lookup";
+    public static final String left_lookup = "Left Lookup";
+    public static final String compare_text = "Compare Text";
+}
